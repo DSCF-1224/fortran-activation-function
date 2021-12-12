b76d5982d994d0415531809f3194b4452f9511e1
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2021-12-12 10:02:20 +0900

    2021.12.12 AM10
    1. fix the implementation of the standard step function

--- src/imp_std_step.f90
+++ src/imp_std_step.f90
@@ -11,7 +11,7 @@ submodule (pkg_activation_function) imp_std_step
     module procedure std_step_real32
 
         if      ( x .gt. 0.0_REAL32 ) then ; std_step = 1.0_REAL32
-        else if ( x .lt. 0.0_REAL32 ) then ; std_step = 0.0_REAL32
+        else if ( x .lt. 0.0_REAL32 ) then ; std_step = 1.0_REAL32
         else                               ; std_step = transfer( source= 0.0_REAL32 , mold= 0_INT32 )
         end if
 
@@ -21,7 +21,7 @@ submodule (pkg_activation_function) imp_std_step
     module procedure std_step_real64
 
         if      ( x .gt. 0.0_REAL64 ) then ; std_step = 1.0_REAL64
-        else if ( x .lt. 0.0_REAL64 ) then ; std_step = 0.0_REAL64
+        else if ( x .lt. 0.0_REAL64 ) then ; std_step = 1.0_REAL64
         else                               ; std_step = transfer( source= 0.0_REAL64 , mold= 0_INT64 )
         end if
 
